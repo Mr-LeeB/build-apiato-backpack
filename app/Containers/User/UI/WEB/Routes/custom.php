930404d615373910706246cd36434c443dcbae01
Mr-LeeB <quangbaokh9a2@gmail.com>
2024-01-03 04:52:01 +0000

    Update user authentication logic

--- app/Containers/User/UI/WEB/Routes/custom.php
+++ app/Containers/User/UI/WEB/Routes/custom.php
@@ -9,16 +9,8 @@ use Illuminate\Support\Facades\Route;
 // Routes you generate using Backpack\Generators will be placed here.
 
 Route::group([
-<<<<<<< HEAD
     
     'namespace' => '\App\Containers\User\UI\WEB\Controllers',
 ], function () { // custom admin routes
     Route::crud('user', 'Controller');
 }); // this should be the absolute last line of this file
-=======
-
-    'namespace' => '\App\Containers\Clients\UI\WEB\Controllers',
-], function () { // custom admin routes
-    Route::crud('clients', 'Controller');
-}); // this should be the absolute last line of this file
->>>>>>> 9069c4e6cd22f4a76d4e61eb3afe450a52abb6e6
